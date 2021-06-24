7de19f5f26cd20595fa28f467718b70c7848da12
Leandro Da Souza <dasouza.leandro@gmail.com>
2021-06-24 15:58:44 +0200

    style content some

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Flex, Text, Heading, Box, Center,  } from '@chakra-ui/layout'
+import { Flex, Text, Heading, Center } from '@chakra-ui/layout'
 import { FaHtml5, FaCss3 } from 'react-icons/fa'
 import { SiJavascript } from 'react-icons/si'
 
 const About = () => {
     return (
-        <Flex w="100vw" flexDirection="column" height="100%">
-            <Flex>
-            <Heading>A little bit about <Text as="span" color="white">me.</Text></Heading>
-            <Text>29 year old man born in Stockholm, Sweden. Started with web development as a hobby in 2016 and began a formal education with workplace experience from 2018 - 2021. What i lack in years of experience i make up with a burning passion for code and development of any kind. Check out my Porfolio for an excerpt of the work i've done or Contact if you want to get in touch!</Text>
+        <Flex w="100vw" flexDirection="column" height="80vh" align="center" justify="space-evenly">
+            <Flex w="100%">
+                <Heading flex="1">A little bit about <Text as="span" color="white">me.</Text></Heading>
+                <Text flex="2">29 year old man born in Stockholm, Sweden. Started with web development as a hobby in 2016 and began a formal education with workplace experience from 2018 - 2021. What I lack in years of experience I make up with a burning passion for code and development of any kind. Check out my <Text as="span" color="white">Porfolio</Text> for an excerpt of the work i've done or <Text as="span" color="white">Contact</Text> if you want to get in touch!</Text>
             </Flex>
             <Center>
-                <SiJavascript/>
-                    <FaHtml5 />
-                    <FaCss3 />
+                <SiJavascript size={25}/>
+                    <FaHtml5 size={25}/>
+                    <FaCss3 size={25}/>
             </Center>
         </Flex>
     )
