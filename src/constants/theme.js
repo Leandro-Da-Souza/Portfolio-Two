3a1a4547985b77d8c3a26a123e0d984b3dc787c7
Leandro Da Souza <dasouza.leandro@gmail.com>
2021-06-24 08:07:47 +0200

    found setting to change color

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,4 +1,15 @@
 import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+
+const styles = {
+    global: props => ({
+      body: {
+        color: mode('red.500', 'blue.100')(props),
+        bg: mode('blue.100', 'red.500')(props),
+      },
+    }),
+  };
 
 const theme = extendTheme({
     fonts: {
@@ -9,7 +20,8 @@ const theme = extendTheme({
         custom: {
             blue: '#004151'
         }
-    }
+    },
+    styles,
 })
 
 
